172cd659e3a0d656f3c402c51a19e375938627eb
Jan Reinhard <jan.reinhard@rwth-aachen.de>
2024-10-27 13:09:31 +0100

    Change loadUser to handle cases where email/name are null

--- backend/src/main/java/org/example/backend/service/WichtelUserService.java
+++ backend/src/main/java/org/example/backend/service/WichtelUserService.java
@@ -13,9 +13,11 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static java.util.Objects.requireNonNullElse;
 import static org.example.backend.util.DTOConverter.fromDTO;
 
 @Service
@@ -53,24 +55,28 @@ public class WichtelUserService implements OAuth2UserService<OAuth2UserRequest, 
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        String provider = userRequest.getClientRegistration().getClientId();
-        String providerId = oAuth2User.getAttribute("sub");
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        Map<String,Object> attr = oAuth2User.getAttributes();
+        String providerId = attr.get("id").toString();
 
-        String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
+        String email = requireNonNullElse(attr.get("email"),"").toString();
+        String name = requireNonNullElse(attr.get("login"),"").toString();
 
         Optional<WichtelUser> userOptional = repo.findByOauthProviderAndOauthId(provider,providerId);
-        WichtelUser user = userOptional.orElseGet(() -> WichtelUser.builder()
-                .email(email)
-                .name(name)
-                .oauthProvider(provider)
-                .oauthId(providerId)
-                .build());
+        if(userOptional.isEmpty()) {
+            WichtelUser user = WichtelUser.builder()
+                    .email(email)
+                    .name(name)
+                    .oauthProvider(provider)
+                    .oauthId(providerId)
+                    .build();
+            repo.save(user);
+        }
 
         return new DefaultOAuth2User(
                 null,
                 oAuth2User.getAttributes(),
-                name
+                "id"
         );
     }
 }
