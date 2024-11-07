eb557b2c2c4266003607bd547ab87e01692a931a
Jan Reinhard <jan.reinhard@rwth-aachen.de>
2024-11-07 10:33:40 +0100

    Change update in WichtelEventService and WichtelUserService to only update fields where the passed DTO is not null

--- backend/src/main/java/org/example/backend/service/WichtelUserService.java
+++ backend/src/main/java/org/example/backend/service/WichtelUserService.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.model.WichtelUser;
 import org.example.backend.model.WichtelUserDTO;
 import org.example.backend.repo.WichtelUserRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Service;
 
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import static org.example.backend.util.DTOConverter.fromDTO;
+import static org.example.backend.util.UpdateUtil.updateIgnoringNulls;
 
 @Service
 @RequiredArgsConstructor
@@ -31,15 +38,15 @@ public class WichtelUserService{
     }
 
     public WichtelUser update(WichtelUserDTO dto,String id){
-        if(repo.findById(id).isEmpty()){
-            throw new NoSuchElementException();
-        }
-        return repo.save(fromDTO(dto,id));
+        WichtelUser user = repo.findById(id).orElseThrow(NoSuchElementException::new);
+        updateIgnoringNulls(fromDTO(dto,id),user);
+        return repo.save(user);
     }
 
     public List<WichtelUser> findAll(){
         return repo.findAll();
     }
+
 
 
 }
---
+++ backend/src/main/java/org/example/backend/util/UpdateUtil.java
@@ -0,0 +1,23 @@
+package org.example.backend.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UpdateUtil {
+    public static void updateIgnoringNulls(Object update,Object original){
+        final BeanWrapper src = new BeanWrapperImpl(update);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> nullAttributes = new HashSet<>();
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) nullAttributes.add(pd.getName());
+        }
+        BeanUtils.copyProperties(update,original,nullAttributes.toArray(new String[0]));
+    }
+}
